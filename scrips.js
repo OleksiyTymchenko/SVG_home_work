71934e29db4318debb7a3af8ecb265ad38e5196e
oleksiy <super.alextim@gmail.com>
2022-04-21 23:44:50 +0300

    Home work

---
+++ scrips.js
@@ -0,0 +1,19 @@
+/*=================== 1 task =================*/
+const Name = prompt("Your name?");
+alert(`Hello` + ` ` + Name);
+
+/*========================= 2 task ====================================== */
+const Year = +prompt("Year of your birthday?");
+const currentYear = 2022;
+
+alert(+currentYear - +Year);
+/*============================ 3 task ==========================*/
+const square = +prompt("Length os square?");
+
+alert(square + square + square + square);
+
+/*=========================== 4 task ====================================*/
+
+const squareOfCircle = +prompt("Radius of circle?");
+
+alert(squareOfCircle * squareOfCircle * 3.14);
